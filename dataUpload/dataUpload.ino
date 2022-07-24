75ae7bbf5b037e53bebfccbab6332b604857574e
Kushagra Goel <64742426+A223D@users.noreply.github.com>
2022-07-23 20:27:37 -0400

    Adds initial files

---
+++ dataUpload/dataUpload.ino
@@ -0,0 +1,14 @@
+#include "secrets.h"
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("print Setup");
+  Serial.println(apiKey);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+}
---
+++ dataUpload/secrets.h
@@ -0,0 +1,2 @@
+#include <Arduino.h>
+extern String apiKey;
