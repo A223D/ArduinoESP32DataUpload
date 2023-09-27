ae96fd3e7c218911a93ff50ab9ed51feb9b55fb5
Bhavesh Kakwani <bk@bhave.sh>
2023-09-27 16:28:54 -0400

    attempt to migrate from old deta to Space

--- webServer/main.py
+++ webServer/main.py
@@ -1,42 +1,45 @@
 import time
 import random
 import json
-from dotenv import load_dotenv
+
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
 from deta import Deta
 from flask import Flask, send_file
 
-deta = Deta(os.getenv('DETA_PROJECT_KEY'))
-db = deta.Base(os.getenv('DETA_BASE_NAME'))
+deta = Deta(os.getenv("DETA_PROJECT_KEY"))
+db = deta.Base(os.getenv("DETA_BASE_NAME"))
 
 app = Flask(__name__)
 
 
-@app.get('/')
+@app.get("/")
 def index():
-  '''main UI page'''
-  return send_file('static/index.html')
+    """main UI page"""
+    return send_file("static/index.html")
 
-@app.get('/reading')
+
+@app.get("/reading")
 def put_reading():
-  '''test helper endpoint that puts a random data point into the db'''
-  db.put({'value': random.randint(0, 4096)}, str(int(time.time())))
-  return 'OK'
+    """test helper endpoint that puts a random data point into the db"""
+    db.put({"value": random.randint(0, 4096)}, str(int(time.time())))
+    return "OK"
 
 
-@app.get('/latest/<timestamp>')
+@app.get("/latest/<timestamp>")
 def get_latest(timestamp: float):
-  '''endpoint to get the latest readings from the data base'''
-  # only show readings greater than the provided unix timestamp
-  resp = db.fetch(query={'key?gt': timestamp}, limit=1)
-  if resp.items:
-    return json.dumps(resp.items)
-  else:
-    return '', 204  # 204 means 'no content in response'
+    """endpoint to get the latest readings from the data base"""
+    # only show readings greater than the provided unix timestamp
+    resp = db.fetch(query={"key?gt": timestamp}, limit=1)
+    if resp.items:
+        return json.dumps(resp.items)
+    else:
+        return "", 204  # 204 means 'no content in response'
+
 
 # run server in debug mode if run directly (for local dev)
-if __name__=='__main__':
-  app.run(debug=True, host='0.0.0.0', port=6969)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=6969)
